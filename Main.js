8af1078713f959c3aa186dec2cd7448e0139cdb1
seirra blake <sophietheopossum@yandex.ru>
2022-03-29 02:19:26 +0100

    fix order

--- Main.js
+++ Main.js
@@ -1,19 +1,19 @@
-var drawing = new Drawing();
-
+let drawing;
 function setup() {
 	createCanvas(windowWidth, windowHeight);
+	drawing = new Drawing();
 }
 
 function draw() {
 	background(255);
 	drawing.draw();
-	
+
 	if(!drawing.on) {
 		textSize(50);
 		strokeWeight(1);
 		text("FailPass".substring(drawing.error>20?0:4, 4 + (drawing.error>20?0:4)) + ": " + drawing.health + " health", 100, 100);
 	}
-	
+
 	if(mouseIsPressed) {
 		drawing.create();
 	}
